61b112998904f3c1c5eca4c9430cecafdb3cde66
Thein Htet Aung <47627954+TheinHtetAung-MM@users.noreply.github.com>
2025-07-06 17:58:10 +0630

    Update ocr.js

--- ocr.js
+++ ocr.js
@@ -1,1 +1,20 @@
+document.getElementById('imageInput').addEventListener('change', function (e) {
+  const file = e.target.files[0];
+  if (!file) return;
+
+  const reader = new FileReader();
+  reader.onload = function () {
+    Tesseract.recognize(
+      reader.result,
+      'mya',
+      {
+        logger: m => console.log(m),
+        langPath: 'https://tessdata.projectnaptha.com/4.0.0/' // auto-load mya.traineddata.gz
+      }
+    ).then(({ data: { text } }) => {
+      document.getElementById('output').innerText = text;
+    });
+  };
+  reader.readAsDataURL(file);
+});
 
